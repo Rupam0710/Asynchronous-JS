77ca430284ce94a6901d8965f0cd7091cdf996f1
Rupam <rupampal48@gmail.com>
2023-11-06 14:59:56 +0530

    6th commit - async await continuation

--- callback.js
+++ callback.js
@@ -10,26 +10,46 @@ let stocks = {
 
 let is_shop_open = true;
 
-let toppings_choice = () => {
+function time(ms) {
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(console.log("which toppings woud you love??"));
-    }, 3000);
+    if (is_shop_open) {
+      setTimeout(resolve, ms);
+    } else {
+      reject(console.log("shop is closed"));
+    }
   });
-};
+}
 
 async function kitchen() {
-  console.log("A");
-  console.log("B");
-  console.log("C");
+  try {
+    await time(2000);
+    console.log(`${stocks.Fruits[0]} was selected`);
 
-  await toppings_choice();
-  console.log("D");
-  console.log("E");
+    await time(0);
+    console.log("Production has started");
+
+    await time(2000);
+    console.log("The fruit was chopped");
+
+    await time(1000);
+    console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was selected`);
+
+    await time(1000);
+    console.log("Start the machine");
+
+    await time(2000);
+    console.log(`Ice cream was placed on ${stocks.holder[0]}`);
+
+    await time(3000);
+    console.log(`${stocks.toppings[0]} was selected`);
+
+    await time(2000);
+    console.log(`Ice cream was served`);
+  } catch (error) {
+    console.log("customer left", error);
+  } finally {
+    console.log("day is over,shop is closed");
+  }
 }
 
 kitchen();
-
-console.log("Doing the dishes");
-console.log("Cleaning the Table");
-console.log("Taking other orders");
